8040fae1fda0f49c67c674498ca76e8aa22d62f8
Dominik Gedon <dominik@gedon.org>
2025-03-14 21:52:10 +0100

    Replace deprecated `::set-output`
    As per the deprecation warning and explanation at
    https://github.blog/changelog/2022-10-11-github-actions-deprecating-save-state-and-set-output-commands/
    
    Signed-off-by: Dominik Gedon <dominik@gedon.org>

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@ output=$(changelog $@)
 output="${output//'%'/'%25'}"
 output="${output//$'\n'/'%0A'}"
 output="${output//$'\r'/'%0D'}"
-echo "::set-output name=result::$output"
+echo "result=$output" >> $GITHUB_OUTPUT
